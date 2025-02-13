6cee38ee014f4df5a1f6a762f511d450695b9a31
Ryan Taylor <rctaylor123@gmail.com>
2025-02-13 08:25:12 -0700

    remove unnecessary files

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -1,13 +1,12 @@
 import { createServerClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
-import { Database } from '@/database.types'
 
 export async function updateSession(request: NextRequest) {
   let supabaseResponse = NextResponse.next({
     request,
   })
 
-  const supabase = createServerClient<Database>(
+  const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
